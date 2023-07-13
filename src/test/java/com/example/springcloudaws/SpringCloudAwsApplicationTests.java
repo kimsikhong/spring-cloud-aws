1355b5f779f920ff70581d01bbb239ea95617e2e
김홍식 <kim.hongsik@postype.com>
2023-07-13 16:40:36 +0900

    initial commit

---
+++ src/test/java/com/example/springcloudaws/SpringCloudAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
